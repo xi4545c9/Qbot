7982918ae5681b44613c49908b7f440fb6d669f3
dogfish <xi4545c9@163.com>
2023-03-15 00:30:18 +0800

    init

---
+++ QbotTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using EleCho.GoCqHttpSdk;
+using EleCho.GoCqHttpSdk.Message;
+namespace QbotTest
+{
+    internal class Program
+    {
+        CqHttpSession session=new CqHttpSession(new CqHttpSessionOptions()
+            {
+                BaseUri = new Uri("http://localhost:5700")
+            }
+        );
+
+        private async void StartSession()
+        {
+
+        }
+        
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
